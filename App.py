86a80959abe0f9ace43efe0eff21f528869c3329
nvtanphat <160033645+nvtanphat@users.noreply.github.com>
2024-09-08 09:54:54 +0700

    App.py

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ x_std = model['x_std']
 theta = model['theta']
 
 @st.cache_resource
-def predict(carat, cut, color, clarity, depth, table, x, y, z):
+def predict(carat, cut, color, clarity, depth, table, x, y, z, x_mean, x_std, theta):
     # Mappings for categorical features
     color_mapping = {'J': 0, 'I': 1, 'H': 2, 'G': 3, 'F': 4, 'E': 5, 'D': 6}
     cut_mapping = {'Fair': 0, 'Good': 1, 'Very Good': 2, 'Premium': 3, 'Ideal': 4}
@@ -20,31 +20,31 @@ def predict(carat, cut, color, clarity, depth, table, x, y, z):
     clarity = clarity_mapping.get(clarity, 0)
 
     # Prepare the input array
-    input = np.array([carat, depth, table, x, y, z, cut, color, clarity])
+    input = np.array([carat, depth, table, x, y, z, cut, color, clarity], dtype = 'float')
     
     # Standardize the input
     input = (input - x_mean) / x_std
 
     # Add a bias term (intercept)
-    input = np.concatenate(([1], input))
-
+    b = np.array([[1.0]])
+    input = np.concatenate((b, input), axis = 1)
     # Perform prediction using the model
     prediction = input.dot(theta)
     return prediction
 
 # Streamlit UI elements
-st.title('Diamond Price Prediction')
+st.title('DIAMOND PRICES PREDICTION')
 
 st.header('Vui lòng hãy nhập thông tin viên kim cương bạn muốn mua:')
-carat = st.number_input('Carat Weight: ', min_value=0.1, max_value=100.0, value=1.0)
+carat = st.number_input('Carat Weight: ', min_value=0.1, max_value=10.0, value=1.0)
 cut = st.selectbox('Cut Rating: ', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
 color = st.selectbox('Color Rating: ', ['J', 'I', 'H', 'G', 'F', 'E', 'D'])
 clarity = st.selectbox('Clarity Rating: ', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
-depth = st.number_input('Diamond Depth Percentage: ', min_value=0.1, max_value=100.0, value=60.0)
-table = st.number_input('Diamond Table Percentage: ', min_value=0.1, max_value=100.0, value=55.0)
-x = st.number_input('Diamond Length (X) in mm: ', min_value=0.1, max_value=100.0, value=5.0)
-y = st.number_input('Diamond Width (Y) in mm: ', min_value=0.1, max_value=100.0, value=5.0)
-z = st.number_input('Diamond Depth (Z) in mm: ', min_value=0.1, max_value=100.0, value=3.0)
+depth = st.number_input('Diamond Depth Percentage: ', min_value=0.1, max_value=100.0, value=1.0)
+table = st.number_input('Diamond Table Percentage: ', min_value=0.1, max_value=100.0, value=1.0)
+x = st.number_input('Diamond Length (X) in mm: ', min_value=0.1, max_value=100.0, value=1.0)
+y = st.number_input('Diamond Width (Y) in mm: ', min_value=0.1, max_value=100.0, value=1.0)
+z = st.number_input('Diamond Depth (Z) in mm: ', min_value=0.1, max_value=100.0, value=1.0)
 
 # Prediction button
 if st.button('Predict Price'):
