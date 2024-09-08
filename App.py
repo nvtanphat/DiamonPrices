56cb3a5885f389a3356952b5c26e553aeac7f490
nvtanphat <160033645+nvtanphat@users.noreply.github.com>
2024-09-08 10:16:35 +0700

    App.py

--- App.py
+++ App.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 
-# Load the model
 model = np.load('weight.npz')
 x_mean = model['x_mean']
 x_std = model['x_std']
@@ -9,30 +8,25 @@ theta = model['theta']
 
 @st.cache
 def predict(carat, cut, color, clarity, depth, table, x, y, z, x_mean, x_std, theta):
-    # Mappings for categorical features
     color_mapping = {'J': 0, 'I': 1, 'H': 2, 'G': 3, 'F': 4, 'E': 5, 'D': 6}
     cut_mapping = {'Fair': 0, 'Good': 1, 'Very Good': 2, 'Premium': 3, 'Ideal': 4}
     clarity_mapping = {'I1': 0, 'SI2': 1, 'SI1': 2, 'VS2': 3, 'VS1': 4, 'VVS2': 5, 'VVS1': 6, 'IF': 7}
-
-    # Convert categorical features to numeric
     cut = cut_mapping.get(cut, 0)
     color = color_mapping.get(color, 0)
     clarity = clarity_mapping.get(clarity, 0)
 
-    # Prepare the input array
     input = np.array([carat, depth, table, x, y, z, cut, color, clarity], dtype='float')
     
-    # Standardize the input
+ 
     input = (input - x_mean) / x_std
 
-    # Add a bias term (intercept)
-    input = np.concatenate(([1.0], input))  # Ensure input is a 1D array
+  
+    input = np.concatenate(([1.0], input)) 
     
-    # Perform prediction using the model
+
     prediction = input.dot(theta)
     return prediction
 
-# Streamlit UI elements
 st.title('DIAMOND PRICES PREDICTION')
 
 st.header('Vui lòng hãy nhập thông tin viên kim cương bạn muốn mua:')
@@ -46,11 +40,9 @@ x = st.number_input('Diamond Length (X) in mm: ', min_value=0.1, max_value=100.0
 y = st.number_input('Diamond Width (Y) in mm: ', min_value=0.1, max_value=100.0, value=1.0)
 z = st.number_input('Diamond Depth (Z) in mm: ', min_value=0.1, max_value=100.0, value=1.0)
 
-# Prediction button
 if st.button('Predict Price'):
     try:
         out = predict(carat, cut, color, clarity, depth, table, x, y, z, x_mean, x_std, theta)
-        # Ensure `out` is a scalar value before formatting
         if np.issubdtype(out.dtype, np.number):
             st.success(f"Giá dự đoán của viên kim cương là: ${out[0]:.2f} USD")
         else:
